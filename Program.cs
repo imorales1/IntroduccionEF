92319e03cd3686ddc8da48106874fba567312737
imorales1 <alejan_dro-morales@hotmail.com>
2022-10-11 23:33:41 -0600

    Se agrego metodo modificar y eliminar

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using projectef;
+using projectef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
@@ -17,7 +18,51 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => {
 });
 
 app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) => {
-    return Results.Ok(dbContext.Tareas.Include(p => p.categoria).Where(p => p.PrioridadTarea == projectef.Models.Prioridad.Baja ));
+    //return Results.Ok(dbContext.Tareas.Include(p => p.categoria).Where(p => p.PrioridadTarea == projectef.Models.Prioridad.Alta ));
+    return Results.Ok(dbContext.Tareas.Include(p => p.categoria));
+});
+
+app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) => {
+    tarea.TareaId = Guid.NewGuid();
+    tarea.FechaCreacion = DateTime.Now;
+    await dbContext.AddAsync(tarea);
+    //Otra manera de hacerlo es la siguiente.
+    //await dbContext.Tareas.AddAsync(tarea);
+
+    await dbContext.SaveChangesAsync();
+    Results.Ok();
+});
+
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) => {
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual != null) {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+
+    return Results.NotFound();
+
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) => {
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if(tareaActual != null) {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
 });
 
 app.Run();
